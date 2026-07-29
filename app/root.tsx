a8f63da7a2bee86828b7d951353d7cc19683d78c
KierenY <107174786+flq367@users.noreply.github.com>
2026-07-29 21:32:31 +0800

    Add files via upload

--- app/root.tsx
+++ app/root.tsx
@@ -50,6 +50,9 @@ export const links: Route.LinksFunction = () => [
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
   { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+  { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+  { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+  { rel: "manifest", href: "/site.webmanifest" },
 ];
 
 import { ThemeProvider } from "./components/theme-provider";
