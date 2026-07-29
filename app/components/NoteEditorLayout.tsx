e5ea458f926c60d0ca187f6bad957c31ef923400
KierenY <107174786+flq367@users.noreply.github.com>
2026-07-29 21:08:20 +0800

    Add files via upload

--- app/components/NoteEditorLayout.tsx
+++ app/components/NoteEditorLayout.tsx
@@ -136,6 +136,7 @@ export function NoteEditorLayout({
                                 previewTheme="github"
                                 preview
                                 className="h-full bg-background!"
+                                style={{ height: "100%" }}
                             />
                         </div>
 
